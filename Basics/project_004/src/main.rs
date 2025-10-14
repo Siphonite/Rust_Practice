d89d485cb278e0d1f4bae1a410c869ef8917ea1f
Aman Kumar <amansinha327@gmail.com>
2025-10-15 01:40:41 +0530

    Convert a hardcoded Celsius temperature to Fahrenheit and print the result.

---
+++ Basics/project_004/src/main.rs
@@ -0,0 +1,6 @@
+// 4. 4. Convert a hardcoded Celsius temperature to Fahrenheit and print the result.
+fn main() {
+    let celsius: f32 = 25.0;
+    let farhenite: f32 = (celsius * 9.0 /5.0) + 32.0;
+    println!("{}°C is equal to {}°F", celsius, farhenite);
+    }
