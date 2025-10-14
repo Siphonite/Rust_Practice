18c4495d4a64cd7ca2e9b9bdb778028e8bd90b6c
Aman Kumar <amansinha327@gmail.com>
2025-10-15 01:57:41 +0530

    Add Fahrenheit to Celsius conversion

--- Basics/project_004/src/main.rs
+++ Basics/project_004/src/main.rs
@@ -1,6 +1,10 @@
-// 4. 4. Convert a hardcoded Celsius temperature to Fahrenheit and print the result.
+// 4. Convert a hardcoded Celsius temperature to Fahrenheit and print the result. Also converting a different hardcoded Fahrenheit temperature 
 fn main() {
     let celsius: f32 = 25.0;
     let farhenite: f32 = (celsius * 9.0 /5.0) + 32.0;
     println!("{}°C is equal to {}°F", celsius, farhenite);
-    }
+
+    let n_farhenite: f32 = 77.0;
+    let n_celsius: f32 = (n_farhenite - 32.0* 5.0) / 9.0;
+    println!("{}°F is equal to {}°C", n_farhenite, n_celsius);
+}
