b85cb61aafe0f1d2eb68a209ed93cb71d085d3f9
Aman Kumar <amansinha327@gmail.com>
2025-10-15 07:43:10 +0530

    Compare two hardcoded numbers and print the larger one.

---
+++ Basics/project_006/src/main.rs
@@ -0,0 +1,11 @@
+// 6. Compare two hardcoded numbers and print the larger one.
+
+fn main() {
+    let x = 29;
+    let y = 45;
+    if x>y {
+        println!("{} is larger than {}.",x,y);
+    } else {
+        println!("{} is larger than {}.",y,x);
+     }
+    }
