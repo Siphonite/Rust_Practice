5156ccb51f38efc118f63aab6c0c65916d8296e6
Aman Kumar <amansinha327@gmail.com>
2025-10-15 13:25:49 +0530

    Declare a boolean variable, toggle its value, and print the result.

---
+++ Basics/project_008/src/main.rs
@@ -0,0 +1,8 @@
+//8. Declare a boolean variable, toggle its value, and print the result.
+
+fn main() {
+    let mut x = true;
+    println!("The value of x is : {}", x); 
+    x = !x; 
+    println!("The value of x after swapping is : {}", x);
+}
