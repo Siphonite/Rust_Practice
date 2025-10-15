ae289f90384728faa6e4d4109d592e486aafd7b3
Aman Kumar <amansinha327@gmail.com>
2025-10-15 07:29:34 +0530

    Check if a hardcoded number is even or odd using the modulo operator.

---
+++ Basics/project_005/src/main.rs
@@ -0,0 +1,10 @@
+// 5. Check if a hardcoded number is even or odd using the modulo operator. 
+
+fn main() { 
+    let x = 63;
+    if x%2 == 0 {
+        println!("{} is an even number.",x);
+    }else {
+        println!("{} is an odd number.",x);
+    }
+}
