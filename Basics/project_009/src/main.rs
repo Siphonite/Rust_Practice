5c32600a6ef0183345a4d0ad87f12a9969dc00f4
Aman Kumar <amansinha327@gmail.com>
2025-10-15 14:29:21 +0530

    Compute the area of a rectangle given its hardcoded length and width.

---
+++ Basics/project_009/src/main.rs
@@ -0,0 +1,11 @@
+// 9. Compute the area of a rectangle given its hardcoded length and width.
+fn main() {
+    let length:f32 = 71.26;
+    let breadth:f32 = 45.12;
+
+    println!("The lenght of rectangle is : {} meters", length);
+    println!("The breadth of rectangle is : {} meters", breadth);
+
+    let area:f32 = length * breadth;
+    println!("\nThe area of rectangle is : {} sq meters", area);
+}
