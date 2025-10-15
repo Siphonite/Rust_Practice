59954fd2608ca9848cfed3ae07c49b28a7419550
Aman Kumar <amansinha327@gmail.com>
2025-10-15 23:48:12 +0530

    3. Calculate the sum of numbers from 1 to 10 using a for loop.

---
+++ Basics/project_013/src/main.rs
@@ -0,0 +1,12 @@
+//13. Calculate the sum of numbers from 1 to 10 using a for loop. 
+
+fn main() {
+
+let mut sum = 0;
+
+ for x in 1..11 { 
+   sum = sum + x;
+ }
+ println!("The sum of all numbers is {}",sum);
+ }
+ 
