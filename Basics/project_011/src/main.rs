44f3c2ab31656ade565563d858772fb617e2eca8
Aman Kumar <amansinha327@gmail.com>
2025-10-15 23:27:59 +0530

    11. Check if a hardcoded number is zero, positive, or negative using if-else.

---
+++ Basics/project_011/src/main.rs
@@ -0,0 +1,16 @@
+// 11. Check if a hardcoded number is zero, positive, or negative using if-else.
+
+fn main() {
+    let a = -45;
+    
+    if a>0 {
+     println!("{} is greater than 0",a);
+     }
+     else if a==0 {
+         println!("{} is equal to 0",a);
+         }
+     else {
+         println!("{} is less than 0",a);
+     }
+
+}
