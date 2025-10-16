903368a6dea8758ae34210585153a7c787164ab4
Aman Kumar <amansinha327@gmail.com>
2025-10-16 09:54:29 +0530

    14. Print even numbers from 1 to 20 using a for loop and if condition.

---
+++ Basics/project_014/src/main.rs
@@ -0,0 +1,9 @@
+//14. Print even numbers from 1 to 20 using a for loop and if condition.
+
+fn main(){
+    for i in 0..21{ 
+        if i%2==0{
+            println!("{} is even",i);
+        }
+    }
+}
