a23ae739ce795e9a48455d8387758a6e4a25a8f4
Aman Kumar <amansinha327@gmail.com>
2025-10-19 21:26:51 +0530

    Check if a hardcoded character is a vowel or consonant using match.

---
+++ Basics/project_017/src/main.rs
@@ -0,0 +1,11 @@
+// Check if a hardcoded character is a vowel or consonant using match.
+
+fn main() {
+    let ch = 'a';
+
+    match ch {
+        'a' | 'e' | 'i' | 'o' | 'u' |
+        'A' | 'E' | 'I' | 'O' | 'U' => println!("The character is a vowel."),
+        _ => println!("The character is a consonant."),
+    }
+}   
