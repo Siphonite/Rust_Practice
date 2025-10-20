234b8fbe280db644d35128a1331773604631f804
Aman Kumar <amansinha327@gmail.com>
2025-10-20 18:31:44 +0530

    Added functionality to read the file

--- my_mini_grep/src/main.rs
+++ my_mini_grep/src/main.rs
@@ -1,10 +1,17 @@
 use std::env;
-fn main () {
-    let args: Vec<String> = env::args().collect();
-   
-   let query   = &args[1];
-   let filepath= &args[2];
+use std::fs;
 
-   println!("Searching for {}", query);
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let filepath = &args[2];
+
+    println!("Searching for {}", query);
     println!("In file {}", filepath);
+
+    let contents = fs::read_to_string(filepath)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{}", contents);
 }
