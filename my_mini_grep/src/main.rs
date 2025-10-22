00d86cc0938de40d45e6e5c1776a6529ff7af963
Aman Kumar <amansinha327@gmail.com>
2025-10-22 14:12:56 +0530

    Fixing the Error Handling

--- my_mini_grep/src/main.rs
+++ my_mini_grep/src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn main() { 
+  
+    let args: Vec<String> = env::args().collect(); 
+    let config = Config::new(&args); 
 
-    let config = parse_config(&args);
-
-    println!("Searching for {}", config.query);
+        println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
     let contents = fs::read_to_string(config.file_path)
@@ -14,16 +14,21 @@ fn main() {
 
     println!("With text:\n{}", contents);
 
-}
+} 
 
 struct Config {
     query: String,
     file_path: String,
 }
+ 
+impl Config {
+        fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
