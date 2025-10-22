b9591c1ee944bda511256c101e56dabdbfdfe5a8
Aman Kumar <amansinha327@gmail.com>
2025-10-22 15:26:21 +0530

    Added search functionality on lib.rs file

--- my_mini_grep/src/main.rs
+++ my_mini_grep/src/main.rs
@@ -1,34 +1,61 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
-fn main() { 
-  
-    let args: Vec<String> = env::args().collect(); 
-    let config = Config::new(&args); 
+use my_mini_grep::{search, search_case_insensitive};
 
-        println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("With text:\n{}", contents);
-
-} 
-
-struct Config {
-    query: String,
-    file_path: String,
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
- 
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
 impl Config {
-        fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
 }
