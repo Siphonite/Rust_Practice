f1b36d5ba0130b520b54ddb53200f2586496a16d
Aman Kumar <amansinha327@gmail.com>
2025-10-24 21:21:12 +0530

    Compute the square of a hardcoded number and print the result.

---
+++ Basics/project_018/src/main.rs
@@ -0,0 +1,7 @@
+// Compute the square of a hardcoded number and print the result.
+
+fn main() {
+    let number = 5;
+    let square = number * number;
+    println!("The square of {} is {}", number, square);
+}
