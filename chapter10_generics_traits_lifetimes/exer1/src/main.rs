295232329ccf440bcdec78f5fe65ee0bdce6d505
Aman Kumar <amansinha327@gmail.com>
2025-11-03 08:03:10 +0530

    Write a function largest<T>(list: &[T]) -> &T that returns the largest element from a list of integers. Then modify it to work for char.

---
+++ chapter10_generics_traits_lifetimes/exer1/src/main.rs
@@ -0,0 +1,21 @@
+// Write a function largest<T>(list: &[T]) -> &T that returns the largest element from a list of integers. Then modify it to work with a list of characters as well. 
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
