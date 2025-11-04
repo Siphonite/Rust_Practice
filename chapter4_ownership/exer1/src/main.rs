9a550bb5b5b52bf5cb17991485ea9fcc9d20fa7c
Aman Kumar <amansinha327@gmail.com>
2025-11-04 08:41:53 +0530

    git update

--- chapter4_ownership/exer1/src/main.rs
+++ chapter4_ownership/exer1/src/main.rs
@@ -1,13 +1,13 @@
 // Write a function takes_ownership(s: String) that prints the string. Call it with a String from main and then try printing the same variable again. // Fix the code so that it works.
 fn main() {
-    let s = String::from("hello");
+    let s = String::from("hello"); // s comes into scope
 
-    let s = takes_ownership(s);
+    let s = takes_ownership(s);  // s's value moves into the function and then is returned back to main
 
-    println!("Back in main: {s}");
+    println!("Back in main: {s}"); // s can still be used here because ownership was returned
 }
 
-fn takes_ownership(s: String) -> String {
-    println!("Inside takes_ownership: {s}");
+fn takes_ownership(s: String) -> String {   // defines a function that takes ownership of a String and returns it
+    println!("Inside takes_ownership: {s}"); // prints the string
     s
 }
