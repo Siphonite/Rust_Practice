acd1b7552c52a5e3c5f2133e623d23a8b1922b63
Aman Kumar <amansinha327@gmail.com>
2025-11-05 10:13:58 +0530

    Use implicit deref coercion: write a function hello(name: &str) and call it with MyBox<String>. It should compile because of deref coercion.

---
+++ chapter15_smart_pointers/exer3/src/main.rs
@@ -0,0 +1,22 @@
+// Use implicit deref coercion: write a function hello(name: &str) and call it with MyBox<String>. It should compile because of deref coercion. 
+
+use std::ops::Deref; 
+struct MyBox<T>(T);
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+} 
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+} 
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+fn main() {
+    let my_boxed_name = MyBox::new(String::from("Rustacean"));
+    hello(&my_boxed_name); // Deref coercion happens here
+}
